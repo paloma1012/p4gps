23746be44c2149f61a9a51e70ea53301f4c9a003
Paloma Cuenca Gil <paloma1012>
2018-01-12 12:32:01 +0100

    TravisCI ahora crea imágenes con los binarios construidos y los sube a Docker Hub

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-while ! curl http://mongo:27017/
+#no lanzar el .jar hasta que no esté accesible el servicio de MongoDB
+#el host ‘mongo’ tendrá una IP otorgada al lanzar la pila de
+#servicios, y le damos el apodo ‘mongo’, utilizado aquí y en el
+#archivo application.propertieswhile
+! curl http://mongo:27017/
 do
-  echo "$(date) - still trying"
-  sleep 1
+ echo "$(date) - still trying"
+ sleep 1
 done
-echo "$(date) - connected successfully"
-
-java -jar ordering-*.jar
+echo "$(date) - it seems mongo server is connected successfully"
